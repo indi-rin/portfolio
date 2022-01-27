6518a0b096cd4adfdd62a7689b88ee7615b12e4f
Indi Rinearson <indi.rinearson@gmail.com>
2022-01-26 22:25:35 -0800

    eject save

--- server/server.ts
+++ server/server.ts
@@ -6,9 +6,15 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
+
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+// });
 
 app.listen(port, () => {
   console.log("Server is up!");
