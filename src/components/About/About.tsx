a3d508058a65c0206c6815acd1a516ed60ff90f6
Indi Rinearson <indi.rinearson@gmail.com>
2023-02-14 19:35:30 -0800

    updated about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -17,20 +17,24 @@ const About: FC = () => {
       <Container>
         <Name>About Indi</Name>
         <Bio>
-          I'm a full-stack software developer based out of the Greater Seattle
-          Area. A long-time lover of logic puzzles, pattern-matching, and
-          storytelling, I bring strong backend capabilities and a keen eye for
-          frontend design to every project I create.
+          I'm a full-stack software developer based out of Seattle. A long-time
+          lover of logic puzzles, pattern-matching, and storytelling, I bring
+          strong backend capabilities and a keen eye for frontend design to
+          every project I create.
         </Bio>
         <Bio>
           My intro to programming began with courses in Lua and Java in high
           school (Lakeside School in Seattle, WA) and continued with C++ in
           college (Kenyon College in Gambier, OH), where I majored in Medieval
-          History and minored in English. Most recently, I graduated the Grace
+          History and minored in English. More recently, I graduated the Grace
           Hopper Program (Fullstack Academy in New York, NY), an immersive
           software engineering program centered on full-stack JavaScript web
           development where I took on the additional challenge of teaching
           myself TypeScript to build a mobile app for my capstone project.
+        </Bio>
+        <Bio>
+          I'm currently working at Axios HQ as a Solutions Engineer, writing
+          TypeScript & React on the frontend and Python & Django on the backend.
         </Bio>
         <Bio>
           Outside of programming, I enjoy baking (pies are my speciality),
